48c3ebc1dbce1742361a5da61ecd54e9587de04e
Mark Menkhus <mark.menkhus@gmail.com>
2014-11-08 15:48:52 -0700

    embellished prototype test script

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+echo "*********** help:"
 ./help
+echo "********** database:"
 ./database
+echo "********** workfile:"
 ./workfile
+echo "********* items:"
 ./items
+echo "********* packagename:"
 ./packagename
+echo "********* version:"
 ./version
+echo "********* build:"
 ./build
