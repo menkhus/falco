7503e83186e7b29354e47733fde32a9820280323
Mark Menkhus <mark.menkhus@gmail.com>
2015-03-29 15:20:41 -0600

    mm - cleanup: clarified test cases, cleaned up mater test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,16 +1,62 @@
 #!/bin/sh
 
-echo "*********** help:"
-./help
+echo "*********** test help:"
+./help > /dev/null
+if [ "$?" -eq 0 ]
+then
+    echo "falco help command line logic test succeeded"
+else
+    echo "falco help command line logic test FAILED"
+    exit
+fi
 echo "********** database:"
-./database
+./database > /dev/null
+if [ "$?" -eq 0 ]
+then
+    echo "falco database argument command line logic test succeeded"
+else
+    echo "falco database argument command line logic test FAILED"
+    exit
+fi
 echo "********** workfile:"
-./workfile
+./workfile > /dev/null
+if [ "$?" -eq 0 ]
+then
+    echo "falco workfile logic test succeeded"
+else
+    echo "falco workfile logic test FAILED"
+    exit
+fi
 echo "********* items:"
-./items
+./items > /dev/null
+if [ "$?" -eq 0 ]
+then
+    echo "falco number of items test logic succeeded"
+else
+    echo "falco number of items test logic test FAILED"
+    exit
+fi
+./packagename > /dev/null
 echo "********* packagename:"
-./packagename
+if [ "$?" -eq 0 ]
+then
+    echo "falco packagename logic test succeeded"
+else
+    echo "falco packagename logic test FAILED"
+    exit
+fi
 echo "********* version:"
-./version
+./version > /dev/null
+if [ "$?" -eq 0 ]
+then
+    echo "falco package version logic test succeeded"
+else
+    echo "falco package version logic test FAILED"
+    exit
+fi
 echo "********* build:"
-./build
+./build  > /dev/null
+if [ "$?" -eq 0 ]
+then
+    echo "all tests succeeded"
+fi
