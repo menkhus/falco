845203e092671a1af6336926e9a85e963838d754
Mark Menkhus <mark.menkhus@gmail.com>
2016-05-30 08:51:06 -0600

    added newer vfeed tree to falco, as always, license is for non commercial use only

--- vfeed/vfeed/__init__.py
+++
@@ -1,7 +0,0 @@
-from .api import vFeed
-from .info import vFeedInfo
-from .exportxml import vFeedXML
-from .update import vFeedUpdate
-from .stats import vFeedStats
-from .search import vFeedSearch
-from . import config  
